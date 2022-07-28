b58c34ccace00c7fd3d893fd39fadb04b936c876
cocopon <cocopon@me.com>
2022-07-28 18:46:41 +0900

    Update sketch

--- public/sketch.js
+++ public/sketch.js
@@ -4,10 +4,12 @@ const PARAMS = {
 		x: 12,
 		y: 16,
 	},
-	noiseScale: .01,
+	noiseScale: .015,
 	fontSize: 12,
 	seed: 0,
 };
+
+const CELLS = [];
 
 // TODO: dark/light
 const ICEBERG = {
@@ -30,20 +32,44 @@ const GRADI = [
 	'▓',
 ];
 
-function trimBody() {
+function setUpText() {
 	PARAMS.text = PARAMS.text.join('')
 		.replace(/[\n\s\t]+/g, '')
 		.toUpperCase();
 }
 
-function step(v) {
-	return v - v % 0.2;
+function colorCells(pat, col) {
+	let tx = PARAMS.text;
+	let oj = 0;
+	let j = 0;
+	do {
+		const m = tx.match(pat);
+		const j = m && m.index;
+
+		if (!m || j < 0) {
+			break;
+		}
+
+		oj += j;
+		if (oj >= CELLS.length) {
+			break;
+		}
+
+		for (let dj = 0; dj < m[0].length; dj++) {
+			if (!CELLS[oj + dj]) {
+				break;
+			}
+			// CELLS[oj + dj].alpha += random(0.5);
+			CELLS[oj + dj].color = col;
+		};
+
+		tx = tx.substring(j + m[0].length, tx.length);
+		oj += m[0].length;
+	} while (true);
 }
 
-function hoge() {
-	background(ICEBERG.bg);
-	textSize(PARAMS.fontSize);
-	noiseSeed(PARAMS.seed);
+function setUpCells() {
+	CELLS.splice(0);
 
 	const h = ceil(height / PARAMS.cell.y);
 	const w = ceil(width / PARAMS.cell.x);
@@ -53,40 +79,95 @@ function hoge() {
 			const x = ix * PARAMS.cell.x;
 			const y = iy * PARAMS.cell.y;
 			const r = noise(x * PARAMS.noiseScale, y * PARAMS.noiseScale, 0);
-			const rr = step(r * r * r);
-			const al = map(rr, 0, 1, 0, 255);
+			const rr = step(pow(r, 4));
+			const cell = {
+				alpha: rr,
+				char: PARAMS.text[i % PARAMS.text.length],
+				color: ICEBERG.fg.normal,
+			};
+
+			CELLS.push(cell);
+		}
+	}
+
+	colorCells(/(for|if|else|const|function|do|while)/i, ICEBERG.fg.blue);
+	colorCells(/(fill|stroke|background|noise)/i, ICEBERG.fg.green);
+	colorCells(/[0-9.]+/, ICEBERG.fg.purple);
+	colorCells(/'.*?'/, ICEBERG.fg.lblue);
+
+	let x = 0;
+	while (x < CELLS.length) {
+		if (random(1) < 0.1 * 4 * CELLS[x].alpha) {
+			CELLS[x].char = GRADI[floor(random(GRADI.length))];
+		}
+		x += 1;
+	}
+}
+
+function step(v) {
+	return v > .5 ? 1 : v > .25 ? .5 : 0;
+	// return v - v % 0.33;
+}
+
+function drawTexts() {
+	textSize(PARAMS.fontSize);
+
+	const h = ceil(height / PARAMS.cell.y);
+	const w = ceil(width / PARAMS.cell.x);
+	for (let iy = 0; iy < h; iy++) {
+		for (let ix = 0; ix < w; ix++) {
+			const i = iy * w + ix;
+			const x = ix * PARAMS.cell.x;
+			const y = iy * PARAMS.cell.y;
+			const cell = CELLS[i];
 			fill(
-				red(ICEBERG.fg.normal),
-				green(ICEBERG.fg.normal),
-				blue(ICEBERG.fg.normal),
-				al,
+				red(cell.color),
+				green(cell.color),
+				blue(cell.color),
+				map(cell.alpha, 0, 1, 0, 255),
 			);
 
-			if (random(1) < .05) {
-				const cols = ['red', 'orange', 'green', 'lblue', 'blue', 'purple'];
-				fill(ICEBERG.fg[cols[floor(random(cols.length))]]);
-			}
-
-			if (rr < 1e-3) {
+			if (cell.alpha < 1e-3) {
 					fill(
-						red(ICEBERG.fg.normal),
-						green(ICEBERG.fg.normal),
-						blue(ICEBERG.fg.normal),
+						red(cell.color),
+						green(cell.color),
+						blue(cell.color),
 						40,
 					);
 					text('·', x + PARAMS.cell.x * 0.5, y + PARAMS.cell.y * 0.5);
 			} else {
-				if (random(1) < 0.1) {
-					const ch = GRADI[floor(random(GRADI.length))];
-					text(ch, x + PARAMS.cell.x * 0.5, y + PARAMS.cell.y * 0.5);
-				} else {
-					// const ch = random(1) < 0.5 ? '·' : String.fromCharCode(['A'.charCodeAt(0) + random(30)]);
-					const ch = PARAMS.text[i % PARAMS.text.length];
-					text(ch, x + PARAMS.cell.x * 0.5, y + PARAMS.cell.y * 0.5);
-				}
+				text(cell.char, x + PARAMS.cell.x * 0.5, y + PARAMS.cell.y * 0.5);
 			}
 		}
 	}
+}
+
+function drawArtwork() {
+	noiseSeed(PARAMS.seed);
+	setUpCells();
+
+	blendMode(BLEND);
+	background(ICEBERG.bg);
+
+	push();
+	{
+		blendMode(ADD);
+		drawTexts();
+		// filter(BLUR, 8);
+
+		fill(
+			red(ICEBERG.bg),
+			green(ICEBERG.bg),
+			blue(ICEBERG.bg),
+			127,
+		);
+		blendMode(BLEND);
+		rect(0, 0, width, height);
+	}
+	pop();
+
+	blendMode(ADD);
+	drawTexts();
 }
 
 function preload() {
@@ -100,11 +181,11 @@ function setup() {
 	noStroke();
 	noiseDetail(8, .65);
 
-	trimBody();
+	setUpText();
 	console.log(PARAMS.text);
 
 	setTimeout(() => {
-		hoge();
+		drawArtwork();
 	}, 200);
 
 	const pane = new Tweakpane.Pane({
@@ -129,7 +210,9 @@ function setup() {
 		max: 1000,
 		step: 1,
 	});
-	pane.on('change', hoge);
+	pane.on('change', () => {
+		drawArtwork();
+	});
 }
 
 function draw() {
